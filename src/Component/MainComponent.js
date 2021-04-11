321bb2d03c34481e1145407ae87fa6e0761b5ed0
avishek arora <avishek.arora.ext@unit4.com>
2021-04-12 00:13:06 +0530

    fixed code management issues.

--- src/Component/MainComponent.js
+++ src/Component/MainComponent.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Login from './LoginComponent';
+import Login from './Login/LoginComponent';
 import Home from './HomeComponent';
-import Register from './RegisterComponent';
+import Register from './Login/RegisterComponent';
 
 export default function Main(){
 
